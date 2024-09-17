ba3cf9d099910e20dd2be6f28172d060e58bdcb5
GanaF4ll <ganafall9498@gmail.com>
2024-09-17 22:26:26 +0200

    🔧 updated product dtos

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,30 @@
-export class CreateProductDto {}
+import { IsString, IsArray } from 'class-validator';
+import { Category } from 'src/types/category';
+import { Image } from 'src/types/image';
+import { Attribute } from 'src/types/attributes';
+
+export class CreateProductDto {
+  @IsString()
+  readonly name: string;
+
+  @IsString()
+  readonly type: string;
+
+  @IsString()
+  readonly description: string;
+
+  @IsString()
+  readonly short_description: string;
+
+  @IsArray()
+  readonly categories: Category[];
+
+  @IsArray()
+  readonly image: Image[];
+
+  @IsArray()
+  readonly attributes: Attribute[];
+
+  @IsArray()
+  readonly default_attributes: Attribute[];
+}
