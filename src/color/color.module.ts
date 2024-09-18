85b26da49028cc0432229ff60109dc6fa2a07bf9
GanaF4ll <ganafall9498@gmail.com>
2024-09-18 10:44:02 +0200

    🎨 imported colors schema and mongoose module in the color module

--- src/color/color.module.ts
+++ src/color/color.module.ts
@@ -2,7 +2,12 @@ import { Module } from '@nestjs/common';
 import { ColorService } from './color.service';
 import { ColorController } from './color.controller';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { Color, ColorSchema } from './schema/color.schema';
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Color.name, schema: ColorSchema }]),
+  ],
   controllers: [ColorController],
   providers: [ColorService],
 })
