826c00affd9377b18d5651cabab3f01eb933cb9d
GanaF4ll <ganafall9498@gmail.com>
2024-09-18 14:17:20 +0200

    category dto

--- src/category/dto/create-category.dto.ts
+++ src/category/dto/create-category.dto.ts
@@ -1,1 +1,15 @@
-export class CreateCategoryDto {}
+import { IsString, IsNumber } from 'class-validator';
+
+export class CreateCategoryDto {
+  @IsNumber()
+  readonly id: number;
+
+  @IsString()
+  readonly name: string;
+
+  @IsNumber()
+  readonly price: number;
+
+  @IsNumber()
+  readonly discount: number;
+}
