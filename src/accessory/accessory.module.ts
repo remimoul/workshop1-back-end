bccc4bebfd850463b88e1be3b8edafbdd7e0aa1f
GanaF4ll <ganafall9498@gmail.com>
2024-09-18 14:54:47 +0200

    accessory module

--- src/accessory/accessory.module.ts
+++ src/accessory/accessory.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AccessoryService } from './accessory.service';
 import { AccessoryController } from './accessory.controller';
+import { Model } from 'mongoose';
+import { MongooseModule, InjectModel } from '@nestjs/mongoose';
+import { Accessory, AccessorySchema } from './schema/accessory.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Accessory.name, schema: AccessorySchema },
+    ]),
+  ],
   controllers: [AccessoryController],
   providers: [AccessoryService],
 })
--- src/accessory/schema/accessory.schema.ts
+++ src/accessory/schema/accessory.schema.ts
@@ -15,3 +15,16 @@ export class Accessory {
   @Prop([Object])
   color: ColorType[];
 }
+
+/**
+ * @description: permet d'avoir un typage précis entre Mongoose et TS,
+ * octroie les méthodes mongoose à l'objet Product
+ */
+export type AccessoryDocument = HydratedDocument<Accessory>;
+
+/**
+ * @param: Class
+ * @description: prend la classe TS avec le décorateur '@Schema()' et ses propriétés avec le décorateur '@prop()'
+ * @returns Un schéma Mongoose qui peut être utilisé pour créer un modèle ou être configuré davantage
+ */
+export const AccessorySchema = SchemaFactory.createForClass(Accessory);
