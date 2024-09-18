46ee22bd7ab3ccce37ab81797dd095da0ca5728e
GanaF4ll <ganafall9498@gmail.com>
2024-09-18 09:34:29 +0200

    ✨ updated appModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ColorModule } from './color/color.module';
 
 @Module({
-  imports: [ColorModule],
+  imports: [
+    //configModule permet d'utiliser le .env sur Nest.JS
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    // configModule est injecter dans le MongooseModule pour pouvoir utiliser les variables d'env dedans
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('LOCAL_DB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ColorModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
