a279068382211cbac021f7c6bfbe806a596e4ed3
GanaF4ll <ganafall9498@gmail.com>
2024-09-18 11:09:18 +0200

    🏷️ update-color-dto

--- src/color/dto/update-color.dto.ts
+++ src/color/dto/update-color.dto.ts
@@ -1,4 +1,17 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateColorDto } from './create-color.dto';
+import { IsString, IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateColorDto extends PartialType(CreateColorDto) {}
+export class UpdateColorDto extends PartialType(CreateColorDto) {
+  @IsString()
+  @IsOptional()
+  name: string;
+
+  @IsString()
+  @IsOptional()
+  code: string;
+
+  @IsBoolean()
+  @IsOptional()
+  isTransparent: boolean;
+}
