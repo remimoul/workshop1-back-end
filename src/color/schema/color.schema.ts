59241ad46b9b28a1d7fcd52134843cc589c549b1
GanaF4ll <ganafall9498@gmail.com>
2024-09-18 10:42:21 +0200

    ✨ color schema

--- src/color/schema/color.schema.ts
+++ src/color/schema/color.schema.ts
@@ -1,5 +1,6 @@
 import { HydratedDocument } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Attribute } from 'src/types/attribute';
 
 @Schema()
 export class Color {
@@ -9,6 +10,18 @@ export class Color {
   @Prop()
   code: string;
 
-  @Prop({ type: [Attribute], required: true })
-  option: string;
+  @Prop({ type: [Object], required: true })
+  option: Attribute[];
 }
+
+/**
+ * @description: permet d'avoir un typage précis entre Mongoose et TS,
+ * octroie les méthodes mongoose à l'objet Product
+ */
+export type ColorDocument = HydratedDocument<Color>;
+/**
+ * @param: Class
+ * @description: prend la classe TS avec le décorateur '@Schema()' et ses propriétés avec le décorateur '@prop()'
+ * @returns Un schéma Mongoose qui peut être utilisé pour créer un modèle ou être configuré davantage
+ */
+export const ColorSchema = SchemaFactory.createForClass(Color);
