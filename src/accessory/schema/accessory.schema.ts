e8be0cd1da17e36e19c1fff8eaa292db94eb0001
GanaF4ll <ganafall9498@gmail.com>
2024-09-18 14:51:00 +0200

    🚧 accessory schema

--- src/accessory/schema/accessory.schema.ts
+++ src/accessory/schema/accessory.schema.ts
@@ -1,11 +1,17 @@
 import { HydratedDocument } from 'mongoose';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Category } from 'src/category/schema/category.schema';
+import * as mongoose from 'mongoose';
+import { ColorType } from 'src/types/color';
 
 @Schema()
 export class Accessory {
   @Prop({ required: true })
   name: string;
 
-  @Prop()
-  category_id: number;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Category' })
+  category: Category;
+
+  @Prop([Object])
+  color: ColorType[];
 }
--- src/types/color.ts
+++ src/types/color.ts
@@ -1,4 +1,5 @@
 import { Image } from './image';
+
 export interface ColorType {
   name: string;
   hexcode: string;
