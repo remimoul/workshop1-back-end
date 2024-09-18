99c6891b4296f11ccfdf8fd8b16f0550016d9851
GanaF4ll <ganafall9498@gmail.com>
2024-09-18 14:55:40 +0200

    🔥 unecessary imports

--- src/accessory/accessory.module.ts
+++ src/accessory/accessory.module.ts
@@ -1,8 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AccessoryService } from './accessory.service';
 import { AccessoryController } from './accessory.controller';
-import { Model } from 'mongoose';
-import { MongooseModule, InjectModel } from '@nestjs/mongoose';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Accessory, AccessorySchema } from './schema/accessory.schema';
 
 @Module({
