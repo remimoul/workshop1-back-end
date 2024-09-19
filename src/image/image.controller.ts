6e3efbdf79c6f26920cf4899f280d981c2cfce44
GanaF4ll <ganafall9498@gmail.com>
2024-09-19 19:44:15 +0200

    🚧 ✨ image routes inc

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -8,8 +8,32 @@ export class ImageController {
   constructor(private readonly imageService: ImageService) {}
 
   @Get(':imageName')
-  getImage(@Param('imageName') imageName: string, @Res() res: Response) {
-    const imagePath = this.imageService.getImagePath(imageName);
+  renderImageFromUploads(
+    @Param('imageName') imageName: string,
+    @Res() res: Response,
+  ) {
+    const imagePath = this.imageService.renderImageFromUploads(imageName);
     return res.sendFile(imagePath);
+  }
+
+  @Get('/back/:imageName')
+  renderImageFromGBCBackFolder(
+    @Param('imageName') imageName: string,
+    @Res() res: Response,
+  ) {
+    const imagePath = this.imageService.renderImageFromGBCBackFolder(imageName);
+    return res.sendFile(imagePath);
+  }
+
+  @Get('back/stickers/all')
+  getStickerUrlFromGBCBackFolder(@Res() res: Response) {
+    const images = this.imageService.getStickerUrlFromGBCBackFolder();
+    return res.json(images);
+  }
+
+  @Get('back/strap/all')
+  getStrapUrlFromGBCBackFolder(@Res() res: Response) {
+    const images = this.imageService.getStrapUrlFromGBCBackFolder();
+    return res.json(images);
   }
 }
--- src/image/image.service.ts
+++ src/image/image.service.ts
@@ -1,9 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { join } from 'path';
+import { readdirSync } from 'fs';
 
 @Injectable()
 export class ImageService {
-  getImagePath(imageName: string): string {
+  ///////////////////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////// TODO: REFACTO/////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////////
+
+  /**
+   * @description: renvoie une image depuis le dossier uploads grâce à son nom
+   * @param imageName
+   * @returns : une image
+   */
+  renderImageFromUploads(imageName: string): string {
     return join(process.cwd(), 'uploads', imageName);
+  }
+
+  /**
+   * @description: renvoie une image depuis le dossier assets/GBC/BACK grâce à son nom
+   * @param imageName
+   * @returns : une image
+   */
+  renderImageFromGBCBackFolder(imageName: string): string {
+    return join(process.cwd(), 'assets/GBC/BACK', imageName);
+  }
+
+  getStickerUrlFromGBCBackFolder(): string[] {
+    const uploadsDir = join(process.cwd(), 'assets/GBC/BACK');
+    const files = readdirSync(uploadsDir);
+    return files.filter((file) => file.includes('STICKER'));
+  }
+
+  getStrapUrlFromGBCBackFolder(): string[] {
+    const uploadsDir = join(process.cwd(), 'assets/GBC/BACK');
+    const files = readdirSync(uploadsDir);
+    return files.filter((file) => file.includes('Strap'));
   }
 }
