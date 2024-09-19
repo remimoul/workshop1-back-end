f3565f59abf129e2aa43073397fd4b2ae31cbb05
GanaF4ll <ganafall9498@gmail.com>
2024-09-19 15:52:03 +0200

    🐛 update accessory dto

--- src/accessory/dto/update-accessory.dto.ts
+++ src/accessory/dto/update-accessory.dto.ts
@@ -1,7 +1,16 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateAccessoryDto } from './create-accessory.dto';
-import { IsArray, IsOptional, IsString, isArray } from 'class-validator';
+import {
+  IsArray,
+  IsBoolean,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+  isArray,
+} from 'class-validator';
 import { ColorType } from 'src/types/color';
+import { Type } from 'class-transformer';
 
 export class UpdateAccessoryDto extends PartialType(CreateAccessoryDto) {
   @IsString()
@@ -12,9 +21,57 @@ export class UpdateAccessoryDto extends PartialType(CreateAccessoryDto) {
 
   @IsArray()
   @IsOptional()
-  readonly variants: ColorType[];
+  @ValidateNested({ each: true })
+  @Type(() => VariantDto)
+  readonly variants: VariantDto[];
 
   @IsString()
   @IsOptional()
   readonly description: string;
+}
+
+class ImageDto {
+  @IsOptional()
+  @IsString()
+  frontViewUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  backViewUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  sideViewUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
+
+class VariantDto {
+  // @IsNumber()
+  // id: number;
+
+  @IsString()
+  name: string;
+
+  @IsString()
+  hexcode: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ImageDto)
+  images: ImageDto[];
+
+  @IsOptional()
+  @IsBoolean()
+  isTransparent?: boolean;
+
+  @IsOptional()
+  @IsBoolean()
+  default: boolean;
+
+  @IsOptional()
+  @IsNumber()
+  price: number;
 }
