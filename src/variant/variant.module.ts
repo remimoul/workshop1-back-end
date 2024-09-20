07891640759d2201ca88f58a0b63d9e0d2fd4de0
GanaF4ll <ganafall9498@gmail.com>
2024-09-20 21:34:41 +0200

    🔧 variant module config

--- src/variant/variant.module.ts
+++ src/variant/variant.module.ts
@@ -2,7 +2,13 @@ import { Module } from '@nestjs/common';
 import { VariantService } from './variant.service';
 import { VariantController } from './variant.controller';
 
+import { MongooseModule } from '@nestjs/mongoose';
+import { Variant, VariantSchema } from './schema/variant.schema';
+
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Variant.name, schema: VariantSchema }]),
+  ],
   controllers: [VariantController],
   providers: [VariantService],
 })
