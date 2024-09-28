2f8961a8bd5d2c10be2066e411759a2af30b1583
GanaF4ll <ganafall9498@gmail.com>
2024-09-28 09:53:09 +0200

    jwtmodule importation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,12 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schema/user.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    JwtModule,
   ],
   controllers: [AuthController],
   providers: [AuthService],
