bb4ed3ebcc316956864e5c349c8a6bb20396bbcd
GanaF4ll <ganafall9498@gmail.com>
2024-09-28 13:11:27 +0200

    ✨ authGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,10 +6,12 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthGuard } from './auth.guard';
 
 @Controller('users')
 export class AuthController {
@@ -35,11 +37,13 @@ export class AuthController {
     return this.authService.findOne(+id);
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
     return this.authService.update(+id, updateAuthDto);
   }
 
+  @UseGuards(AuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.authService.remove(+id);
