235e1353e8344739bf9b062a219167187f1b88e3
GanaF4ll <ganafall9498@gmail.com>
2024-09-28 09:48:13 +0200

    ✨ auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,26 +1,96 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  BadRequestException,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { User } from './schema/user.schema';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signup(signupDto: CreateAuthDto): Promise<User> {
+    const { email, password } = signupDto;
+    const user = await this.userModel.findOne({ email });
+
+    if (user) throw new BadRequestException('Unable to create the user');
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new this.userModel({ email, password: hashedPassword });
+
+    if (!newUser) throw new BadRequestException('Unable to create the user');
+
+    return newUser;
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async login(loginDto: CreateAuthDto): Promise<{ access_token: string }> {
+    const { email, password } = loginDto;
+    const user = await this.userModel.findOne({ email });
+
+    if (!user)
+      throw new BadRequestException('Wrong combination of email and password');
+
+    const isMatch = await bcrypt.compare(password, user.email);
+
+    if (!isMatch)
+      throw new BadRequestException('Wrong combination of email and password');
+
+    const payload = { id: user.id, email: user.email };
+
+    return {
+      access_token: await this.jwtService.signAsync(payload),
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  async findAll(): Promise<User[]> {
+    const users = await this.userModel.find();
+
+    if (!users) throw new NotFoundException(`No users found`);
+
+    return users;
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
+  async findOne(id: number): Promise<User> {
+    const user = await this.userModel.findById(id);
+
+    if (!user) throw new NotFoundException(`No users found with the id ${id}`);
+
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+  async update(id: number, updateAuthDto: UpdateAuthDto): Promise<User> {
+    const user = await this.userModel.findById(id);
+
+    if (!user) throw new NotFoundException(`No user found with the id n°${id}`);
+
+    const updatedUser = await this.userModel.findByIdAndUpdate(
+      id,
+      updateAuthDto,
+      { new: true },
+    );
+
+    return updatedUser;
+  }
+
+  async remove(id: number): Promise<User> {
+    const user = await this.userModel.findById(id);
+
+    if (!user) throw new NotFoundException(`No user found with the id n°${id}`);
+
+    const deletedUser = await this.userModel.findByIdAndDelete(id);
+
+    return deletedUser;
   }
 }
--- src/auth/schema/user.schema.ts
+++ src/auth/schema/user.schema.ts
@@ -7,7 +7,7 @@ export class User {
   @Prop({ required: true })
   email: string;
 
-  @Prop({ repquired: true })
+  @Prop({ required: true })
   password: string;
 }
 
